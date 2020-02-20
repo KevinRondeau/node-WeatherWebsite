edc4dcd9354a3433f899c2bbd5db195601a7cf5f
KevinRondeau <k.rondeau1991@hotmail.com>
2020-02-20 18:31:17 -0500

    New data on forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -28,7 +28,8 @@ const forecast = (latitude, longitude, callback) => {
                 body.daily.data[0].summary +
                 ' It is currently ' + body.currently.temperature +
                 ' degrees out. And there is a ' + body.currently.precipProbability +
-                ' % chance of rain')
+                ' % chance of rain.' + 'The highs for today is :' + body.daily.data[0].temperatureHigh +
+                ' And the low is : ' + body.daily.data[0].temperatureLow)
         }
     })
 }
